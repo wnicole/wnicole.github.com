f8635684bce76edd3ed122350fa9bde773cbfca0
wnicole <worthington.nicole@gmail.com>
2017-09-26 17:24:54 -0700

    disabling submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(function() {
-
+	
+	// reCAPTCHA submit button disable
+	document.getElementById("sendEmailBtn").disabled = true;
+	
+	 function enableBtn(){
+    		document.getElementById("sendEmailBtn").disabled = false;
+   	}
+	
+	
 	// util functions
 	function validateEmail(email) {
 		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
